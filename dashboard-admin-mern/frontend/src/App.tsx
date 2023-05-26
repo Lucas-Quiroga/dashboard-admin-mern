a047d86a97dcc38e4402b7bf548870a23ace134a
Lucas-Quiroga <quiroga.lucasoffice@gmail.com>
2023-05-25 23:11:40 -0300

    retocando la carpeta services + viendo posible solucion al toast de error en la registración

--- dashboard-admin-mern/frontend/src/App.tsx
+++ dashboard-admin-mern/frontend/src/App.tsx
@@ -1,5 +1,4 @@
-import { useState, useEffect } from "react";
-import axios from "axios";
+import { useEffect } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Home from "./Home/components/Home";
@@ -7,81 +6,23 @@ import ModelRegister from "./Register/components/ModelRegister";
 import ModelLogin from "./Login/components/ModelLogin";
 import HomeLogin from "./Home/components/HomeLogin";
 import { Spinner } from "react-bootstrap";
-
-interface User {
-  email: string;
-  password: string;
-  confirmPassword?: string;
-}
+import { useServices } from "./services/useServices";
 
 function App() {
-  const [authenticated, setAuthenticated] = useState(false);
-  const [userEmail, setUserEmail] = useState("");
-  const [userError, setUserError] = useState(false);
-  const [userErrorCount, setUserErrorCount] = useState(0);
-  const [errorUserOffice, setErrorUserOffice] = useState(false);
-  const [showLogoutToast, setShowLogoutToast] = useState(false);
-  const [showRegisterToast, setShowRegisterToast] = useState(false);
-  const [spinnerState, setSpinnerState] = useState(false);
-
-  const handleLogin = async (user: User) => {
-    try {
-      const response = await axios.post("http://localhost:8080/login", user);
-      console.log(response.data);
-
-      if (response.data.success) {
-        setAuthenticated(true);
-        const useremail = response.data.email;
-        setUserEmail(useremail);
-        setSpinnerState(true);
-        setTimeout(() => {
-          setSpinnerState(false);
-        }, 2000);
-      } else {
-        setAuthenticated(false);
-        setUserError(true);
-        setUserErrorCount(userErrorCount + 1);
-        setErrorUserOffice(true);
-      }
-    } catch (error) {
-      console.log(error);
-      setUserError(true);
-      setUserErrorCount(userErrorCount + 1);
-      setErrorUserOffice(true);
-    }
-  };
-
-  const handleRegister = async (user: User) => {
-    try {
-      const response = await axios.post("http://localhost:8080/register", user);
-      if (response.status === 200) {
-        setShowRegisterToast(true);
-      } else {
-        setShowRegisterToast(false);
-      }
-    } catch (error) {
-      console.log(error);
-      setShowRegisterToast(false);
-    }
-  };
-
-  const handleLogout = async () => {
-    try {
-      const response = await axios.post("http://localhost:8080/logout");
-      if (response.status === 200) {
-        setAuthenticated(false);
-        setShowLogoutToast(true);
-        setSpinnerState(true);
-        setTimeout(() => {
-          setSpinnerState(false);
-        }, 2000);
-      } else {
-        setAuthenticated(true);
-      }
-    } catch (error) {
-      console.log("el error es" + error);
-    }
-  };
+  const {
+    authenticated,
+    userEmail,
+    userError,
+    userErrorCount,
+    errorUserOffice,
+    spinnerState,
+    showRegisterToast,
+    showLogoutToast,
+    setErrorUserOffice,
+    handleLogin,
+    handleRegister,
+    handleLogout,
+  } = useServices();
 
   useEffect(() => {
     if (userError && userErrorCount > 0) {
---
+++ dashboard-admin-mern/frontend/src/services/useServices.tsx
@@ -0,0 +1,93 @@
+import { useState } from "react";
+import axios from "axios";
+
+interface User {
+  email: string;
+  password: string;
+  confirmPassword?: string;
+}
+
+export const useServices = () => {
+  const [authenticated, setAuthenticated] = useState(false);
+  const [userEmail, setUserEmail] = useState("");
+  const [userError, setUserError] = useState(false);
+  const [userErrorCount, setUserErrorCount] = useState(0);
+  const [errorUserOffice, setErrorUserOffice] = useState(false);
+  const [spinnerState, setSpinnerState] = useState(false);
+  const [showLogoutToast, setShowLogoutToast] = useState(false);
+  const [showRegisterToast, setShowRegisterToast] = useState(false);
+
+  const handleLogin = async (user: User) => {
+    try {
+      const response = await axios.post("http://localhost:8080/login", user);
+      console.log(response.data);
+
+      if (response.data.success) {
+        setAuthenticated(true);
+        const useremail = response.data.email;
+        setUserEmail(useremail);
+        setSpinnerState(true);
+        setTimeout(() => {
+          setSpinnerState(false);
+        }, 2000);
+      } else {
+        setAuthenticated(false);
+        setUserError(true);
+        setUserErrorCount(userErrorCount + 1);
+        setErrorUserOffice(true);
+      }
+    } catch (error) {
+      console.log(error);
+      setUserError(true);
+      setUserErrorCount(userErrorCount + 1);
+      setErrorUserOffice(true);
+    }
+  };
+
+  const handleRegister = async (user: User) => {
+    try {
+      const response = await axios.post("http://localhost:8080/register", user);
+      if (response.status === 200) {
+        setShowRegisterToast(true);
+      } else {
+        setShowRegisterToast(false);
+      }
+    } catch (error) {
+      console.log(error);
+      setShowRegisterToast(false);
+    }
+  };
+
+  const handleLogout = async () => {
+    try {
+      const response = await axios.post("http://localhost:8080/logout");
+      if (response.status === 200) {
+        setAuthenticated(false);
+        setShowLogoutToast(true);
+        setSpinnerState(true);
+        setTimeout(() => {
+          setSpinnerState(false);
+        }, 2000);
+      } else {
+        setAuthenticated(true);
+      }
+    } catch (error) {
+      console.log("el error es" + error);
+    }
+  };
+
+  return {
+    authenticated,
+    userEmail,
+    userError,
+    userErrorCount,
+    errorUserOffice,
+    spinnerState,
+    showRegisterToast,
+    showLogoutToast,
+    setErrorUserOffice,
+    handleLogin,
+    handleRegister,
+    handleLogout,
+  };
+};
